4dcac62a6538bcd6aa34c588fc1c0342415375bb
NITIN TRIPATHI <224832709+nitintripathi2315-ctrl@users.noreply.github.com>
2026-07-07 00:19:12 +0530

    Time: 181 ms (5.2%), Space: 11.8 MB (16.04%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,46 +1,35 @@
 class Solution {
 public:
-bool fun(vector<int> &have, vector<int> &need)
-{
-    for(int i=0;i<256;i++)
-    {
-        if(have[i]<need[i])
-        return false;
+    bool func(vector <int> &have, vector <int> & need){
+        for(int i=0; i<256; i++){
+            if(have[i] < need[i]){
+                return false;
+            }
+        }return true;
     }
-    return true;
-}
     string minWindow(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        vector<int> have(256,0);
-        vector<int> need(256,0);
+        int n = s.size();
+        int m = t.size();
+        vector <int> have(256,0);
+        vector <int> need(256,0);
         int i;
-        if(n<m)
-        return "";
-        for(i=0;i<m;i++)
-        need[t[i]]++;
-
-        int low=0,high=0;
-        int res=INT_MAX;
-        int start=-1;
-        for(high=0;high<n;high++)
-        {
+        if(n<m) return "";
+        for(int i = 0; i<m; i++)
+            need[t[i]]++; //pta kr lo chaiye kya
+        int low=0, high=0, res = INT_MAX;
+        int start = -1; //to store best at idx = 0
+        for(high=0; high<n; high++){
             have[s[high]]++;
-
-            while(fun(have,need)) // jab tk sahi hai
-            {
-                int len=high-low+1;
-                if(res>len)
-                {
-                    res=len;
-                    start=low;
+            while(func(have, need)){
+                int len = high - low +1;
+                if(res>len){
+                    res = len;
+                    start = low; //to store best at idx=0
                 }
                 have[s[low]]--;
                 low++;
-            }
-        }
-        if(res==INT_MAX)
-        return "";
-        return s.substr(start,res);
+            } 
+        }if(res==INT_MAX) return "";
+        return s.substr(start, res);
     }
 };
