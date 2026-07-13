987f0b9205a3a4ba627e78b4098bc8ce4a0fddad
NITIN TRIPATHI <224832709+nitintripathi2315-ctrl@users.noreply.github.com>
2026-07-13 11:02:03 +0530

    Time: 0 ms (100%), Space: 9.1 MB (78.52%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -3,10 +3,10 @@ public:
     char findTheDifference(string s, string t) {
         int sum =0;
         for(int i=0; i<t.size(); i++){
-            sum += t[i];
+            sum ^= t[i];
         }
         for(int i =0; i<s.size(); i++){
-            sum -= s[i];
-        }return (char)sum;
+            sum ^= s[i];
+        }return sum;
     }
 };
