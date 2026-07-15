cff98e6c80f3fe89167a22ebb0936057b70966ea
NITIN TRIPATHI <224832709+nitintripathi2315-ctrl@users.noreply.github.com>
2026-07-15 15:32:34 +0530

    Time: 0 ms (100%), Space: 8.4 MB (15.51%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool checkRecord(string s) {
-        int  absent =0, streak =0, maxstreak=0;
-        for(int i=0 ; i<s.size(); i++){
+        int absent = 0 , streak = 0 , maxstreak = 0;
+        for(int i =0 ; i<s.size(); i++){
             if(s[i] == 'A') absent++;
-            if(s[i] ==  'L'){
+            if(s[i] == 'L'){
                 streak++;
-                maxstreak = max(maxstreak , streak);
+            maxstreak = max(maxstreak , streak);
+            } else{
+                streak = 0;
             }
-                else{
-                    streak = 0;
-                    }
         }return (absent < 2 && maxstreak < 3);
     }
 };
