44c8d34c84ce35f750f917dd42309cab9c432dd2
NITIN TRIPATHI <224832709+nitintripathi2315-ctrl@users.noreply.github.com>
2026-07-15 16:24:18 +0530

    Time: 0 ms (100%), Space: 31.2 MB (67.92%) - LeetHub

---
+++ 3978-unique-middle-element/3978-unique-middle-element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isMiddleElementUnique(vector<int>& nums) {
+        int mid = nums.size()/2;
+        for(int i=0;i<nums.size(); i++){
+            if(i != mid && nums[i] == nums[mid]){
+                 return false;
+            }
+        }return true;
+    }
+};
