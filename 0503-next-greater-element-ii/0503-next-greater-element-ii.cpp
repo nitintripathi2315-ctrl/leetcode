d8a82f26d910bfef5bd66744b0301fdf2568e942
NITIN TRIPATHI <224832709+nitintripathi2315-ctrl@users.noreply.github.com>
2026-07-25 21:32:42 +0530

    Time: 5 ms (44.85%), Space: 28.5 MB (64.38%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        stack<int> st;   // stores indices
+
+        for (int i = 2 * n - 1; i >= 0; i--) {
+
+            while (!st.empty() && nums[st.top()] <= nums[i % n]) {
+                st.pop();
+            }
+
+            if (!st.empty()) {
+                ans[i % n] = nums[st.top()];
+            }
+
+            st.push(i % n);
+        }
+
+        return ans;
+    }
+};
