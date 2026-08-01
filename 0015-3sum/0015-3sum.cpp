89333d7d3861f2bc8da34841ca87c660d6fca8a0
NITIN TRIPATHI <224832709+nitintripathi2315-ctrl@users.noreply.github.com>
2026-08-01 12:01:42 +0530

    Time: 35 ms (98.93%), Space: 29.2 MB (29.7%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& a) {
-        vector<vector<int>> result;
-        sort(a.begin(),a.end());
-        int n=a.size();
+        sort(a.begin(), a.end());
+        int n =a.size();
+        vector<vector<int>> ans;
         for(int i=0; i<n-2; i++){
-            if(i>0 && a[i]==a[i-1]){
+            if(i>0 && a[i] == a[i-1]){
                 continue;
             }
-            int left = i+1;
+            int left = i+1; 
             int right = n-1;
-            int sum = -a[i]; //a[i] x + y = 0 
+            int sum = -a[i];
         
-        while(left < right ){
-            int s = a[left]  + a[right];
+        while(left < right){
+            int s = a[left] + a[right];
             if(s==sum){
-                result.push_back({a[i], a[left], a[right]});
-                left++;
-                right--;
+                ans.push_back({a[i] , a[left] , a[right]});
             
-            while(left<n && a[left]==a[left-1]){
+            left++;
+            right--;
+            
+            while(left<n && a[left] == a[left-1]){
                 left++;
             }
-            while(right >=0 && a[right]==a[right+1]){
-                right--;
-            }
-            }
-            else if(s<sum){
-                left++;
-            }
-            else{
+            while(right >=0 && a[right] == a[right+1])
+            {
                 right--;
             }
         }
-        }return result;
+            else if(s<sum)
+            {
+                left++;
+            }
+            else right--;
+        }
+        }return ans;
     }
 };
