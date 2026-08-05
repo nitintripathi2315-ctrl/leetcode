43ee390c1ca45a3672cfa655d5fcb87183a68793
NITIN TRIPATHI <224832709+nitintripathi2315-ctrl@users.noreply.github.com>
2026-08-05 20:29:40 +0530

    Time: 0 ms (100%), Space: 11.3 MB (20.71%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+#define Node ListNode
+
+    Node* reverse(Node* head, int times) {
+        Node* curr = head;
+        Node* prev = NULL;
+
+        while (times-- && curr) {
+            Node* nex = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nex;
+        }
+
+        // Connect the last node of reversed list to remaining list
+        head->next = curr;
+
+        return prev;
+    }
+
+    Node* swapPairs(Node* head) {
+
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        Node* left = head;
+        Node* ans = NULL;
+        Node* prevLeft = NULL;
+
+        while (left) {
+
+            // Find second node
+            Node* right = left;
+            int cnt = 1;
+
+            while (cnt < 2 && right) {
+                right = right->next;
+                cnt++;
+            }
+
+            // Less than 2 nodes left
+            if (right == NULL) {
+                if (prevLeft)
+                    prevLeft->next = left;
+                break;
+            }
+
+            Node* nextLeft = right->next;
+
+            // Reverse two nodes
+            Node* newHead = reverse(left, 2);
+
+            if (ans == NULL)
+                ans = newHead;
+
+            if (prevLeft)
+                prevLeft->next = newHead;
+
+            prevLeft = left;
+            left = nextLeft;
+        }
+
+        return ans;
+    }
+};
