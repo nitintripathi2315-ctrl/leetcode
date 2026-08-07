b95d04fc1ca8a54516b2ee886988b015af7be86f
NITIN TRIPATHI <224832709+nitintripathi2315-ctrl@users.noreply.github.com>
2026-08-07 19:15:13 +0530

    Time: 0 ms (100%), Space: 31.2 MB (82.37%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& a, int target) {
+        int n = a.size();
+        int low = 0 , high = n-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(a[mid] == target){
+                return mid;
+            }
+            else if(a[mid]<target){
+                low = mid +1;
+            }
+            else{
+                high =mid-1;
+            }
+        }return -1;
+    }
+};
