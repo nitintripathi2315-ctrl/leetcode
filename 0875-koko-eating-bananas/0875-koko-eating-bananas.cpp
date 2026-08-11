542d7007ebfc716c44cc1066531d5b6918fb815f
NITIN TRIPATHI <224832709+nitintripathi2315-ctrl@users.noreply.github.com>
2026-08-11 10:42:29 +0530

    Time: 10 ms (39.63%), Space: 23 MB (20.62%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,31 +1,31 @@
 #include<algorithm>
 class Solution {
 public:
-    long long func(vector <int> &a , int &n , int &speed){
+ long long func(vector <int> &a , int n , int speed){
         long long hours = 0;
         for(int i=0; i<n; i++){
-            hours +=   (a[i] / speed);
-            if(a[i] % speed != 0){
-                hours++;
-            }
+            hours += a[i] / speed;
+            if(a[i] % speed != 0)
+            hours++;
         }return hours;
     }
+
     int minEatingSpeed(vector<int>& a, int h) {
         int ans = -1;
         int n = a.size();
-        int low = 1;
+        int low =1;
         int high = *max_element(a.begin(), a.end());
-            while(low<=high){
-                int mid = low + (high - low)/2;
-                long long hours = func(a , n , mid);
-                if(hours > h){
-                    low = mid +1;
-                }
-                else{
-                    ans = mid;
-                    high = mid -1;
-                }
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            long long hours = func(a , n , mid);
+            if(hours > h){
+                low = mid +1;
             }
-    return ans;
+            else{
+                ans = mid;
+                high = mid -1;
+            }
+        }return ans;
+   
     }
 };
