b04d29ba13d163b20a7183e8f6f7dd127fd9034b
NITIN TRIPATHI <224832709+nitintripathi2315-ctrl@users.noreply.github.com>
2026-08-15 19:49:10 +0530

    Time: 3 ms (63.11%), Space: 18.1 MB (47.48%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    struct cmp{
+        bool operator()(pair <int , string> &a, pair <int, string> &b){
+            if(a.first == b.first)
+                return a.second < b.second;
+                return a.first > b.first;
+        }
+    };
+    vector<string> topKFrequent(vector<string>& a, int k) {
+        priority_queue<pair<int, string>, vector<pair<int, string>>, cmp>pq;
+        int n = a.size();
+        unordered_map <string, int> f;
+        for(int i=0; i<n; i++){
+            f[a[i]]++;
+        }
+        for(auto x : f){
+            pq.push({x.second, x.first});
+        
+        if(pq.size()>k){
+            pq.pop();
+        }
+        }
+        vector <string> ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
